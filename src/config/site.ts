c5fbed3db94dc0d1894634c560ee280cbf00fdbb
Fabbio Protopapa <fprotopapa.git@gmail.com>
2025-11-25 18:25:41 +0100

    remove language switcher

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@
 
 export const siteConfig = {
   // Enable or disable the language switcher in navigation
-  enableLanguageSwitcher: true,
+  enableLanguageSwitcher: false,
 
   // Enable or disable language switcher on blog posts (for translations)
   enablePostTranslationLinks: false,
